0c9ac5c42ca4d2b8bf60da178cd86b42f81f797e
ophilar <ophilar@gmail.com>
2025-07-10 13:33:22 +0300

    Refactor: Remove Unnecessary Newline in MainActivity and Fix XML Formatting
    This commit includes two minor changes:
    
    *   **`MainActivity.kt`**: Removed an extra blank line in the `onCreate` method.
    *   **`ic_sync_devices.xml`**: Corrected a typo in the XML by removing an extraneous period after `android` in the `strokeLineCap` attribute of the last path.

--- app/src/main/java/com/fluxzen/legatopages/MainActivity.kt
+++ app/src/main/java/com/fluxzen/legatopages/MainActivity.kt
@@ -21,7 +21,6 @@ class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
 
-        
         CoroutineScope(Dispatchers.IO).launch {
             CacheManager(applicationContext).cleanCache()
         }
